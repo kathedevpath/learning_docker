742f56d7f9e1a69aab4613e0ad5c60c96689e553
Kathe <kasia@MacBook-Air-Tobiasz.local>
2023-03-09 14:01:59 +0100

    Create CustomUserModel

---
+++ accounts/models.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = [
+        "first_name",
+        "last_name",
+    ]
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        verbose_name = "user"
+        verbose_name_plural = "users"
