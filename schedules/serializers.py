5acc108c25042c9dc3bf96cfe3849b6263f86fc2
Kathe <kasia@MacBook-Air-Tobiasz.local>
2023-03-15 12:41:26 +0100

    serializers and views for DayPlan list and detail

--- schedules/serializers.py
+++ schedules/serializers.py
@@ -5,11 +5,5 @@ from .models import DayPlan
 
 class DayPlanSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = (
-            "day",
-            "child",
-            "meals_at_school",
-            "behaviour",
-            "summary",
-        )
         model = DayPlan
+        fields = "__all__"
--- schedules/views.py
+++ schedules/views.py
@@ -1,7 +1,13 @@
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from .models import DayPlan
 from .serializers import DayPlanSerializer
+
+"""We need two views:
+detail view for each day for each Child -> parent/teacher
+list view for requested day for every Children ->teacher"""
 
 
 class DayPlanDailyListView(generics.ListCreateAPIView):
@@ -9,6 +15,8 @@ class DayPlanDailyListView(generics.ListCreateAPIView):
     serializer_class = DayPlanSerializer
 
 
-class DayPlanDetailView(generics.RetrieveUpdateAPIView):
-    queryset = DayPlan.objects.all()
-    serializer_class = DayPlanSerializer
+class DayPlanDetailView(APIView):
+    def get(self, request, pk, day):
+        day_plan = DayPlan.objects.get(child_id=pk, day=day)
+        serializer = DayPlanSerializer(day_plan)
+        return Response(serializer.data)
