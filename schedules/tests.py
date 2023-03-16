d57310cd121f27148d1f70f3171d439d3b4a1fe2
Kathe <kasia@MacBook-Air-Tobiasz.local>
2023-03-16 11:18:41 +0100

    Create unittests for Member and DayPlan models

--- schedules/tests.py
+++ schedules/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+from datetime import date
 
-# Create your tests here.
+from members.models import Child, Parent
+from .models import DayPlan
+
+
+class DayPlanTests(TestCase):
+    @classmethod
+    def setUp(cls):
+        cls.user = get_user_model().objects.create_user(
+            email="testparent@mail.com", password="secret"
+        )
+
+        cls.parent = Parent.objects.create(user=cls.user)
+        cls.child = Child.objects.create(
+            full_name="Test Child",
+            birth_date=date(2022, 3, 6),
+            parent=cls.parent,
+        )
+
+        cls.dayplan = DayPlan.objects.create(
+            child=cls.child,
+            day=timezone.now().date(),
+            summary="everything ok",
+        )
+
+    def test_dayplan_model(self):
+        self.assertEqual(self.dayplan.child.full_name, "Test Child")
+        self.assertEqual(self.dayplan.summary, "everything ok")
+        self.assertEqual(self.dayplan.day, timezone.now().date())
