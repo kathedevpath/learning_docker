3419f9443cde7471f55524409a5ec720d795276d
Kathe <kasia@MacBook-Air-Tobiasz.local>
2023-03-16 10:06:53 +0100

    Permissions for dayplan detail and list view

--- schedules/views.py
+++ schedules/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 
 from .models import DayPlan
 from .serializers import DayPlanSerializer
+from .permissions import ParentOnlyViewAndTeacherEdit, OnlyStaffCanSeeListViews
 
 """We need two views:
 detail view for each day for each Child -> parent/teacher
@@ -11,12 +12,16 @@ list view for requested day for every Children ->teacher"""
 
 
 class DayPlanDailyListView(generics.ListCreateAPIView):
+    permission_classes = [OnlyStaffCanSeeListViews]
     queryset = DayPlan.objects.all()
     serializer_class = DayPlanSerializer
 
 
 class DayPlanDetailView(APIView):
+    permission_classes = [ParentOnlyViewAndTeacherEdit]
+
     def get(self, request, pk, day):
         day_plan = DayPlan.objects.get(child_id=pk, day=day)
         serializer = DayPlanSerializer(day_plan)
+        self.check_object_permissions(request, day_plan)
         return Response(serializer.data)
