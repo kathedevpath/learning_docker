19823c8def3a17a988fd0980280c80352386e18f
Kathe <kasia@MacBook-Air-Tobiasz.local>
2023-05-10 11:55:56 +0200

    new app for sending messages

---
+++ chats/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from accounts.models import CustomUser
+
+from members.models import Child
+
+#for single instance
+class Message(models.Model):
+    sender = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='sent_messages',
+    )
+
+    child = models.ForeignKey(Child, on_delete=models.CASCADE, null=True, blank=True)
+    message_text = models.CharField(max_length=1000)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"about {self.child}"
+    
+    class Meta:
+        ordering = ('timestamp',)
+
+
+
