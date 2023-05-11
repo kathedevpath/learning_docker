764be9c77ff0b69c856e606c336b21275ec1b5ad
Kathe <kasia@MacBook-Air-Tobiasz.local>
2023-05-11 12:21:41 +0200

    Adding custom permissions, form for create view and correct filtering in views

--- chats/serializers.py
+++ chats/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Message
+from members.models import Child
 
 
 class MessageSerializer(serializers.ModelSerializer):
@@ -7,8 +8,17 @@ class MessageSerializer(serializers.ModelSerializer):
         read_only=True,
         default=serializers.CurrentUserDefault(),
     )
+    #empty queryset to get information from get_serialized_context
+    child = serializers.PrimaryKeyRelatedField(queryset=Child.objects.none())
     message_text = serializers.CharField(max_length=1000)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if 'children' in self.context:
+            self.fields['child'].queryset = self.context['children']
 
     class Meta:
         model = Message
-        fields = ('id', 'sender', 'child', 'message_text', 'timestamp')
+        fields = ['id', 'sender', 'child', 'message_text', 'timestamp']
+        read_only_fields = ['id', 'timestamp']
+
